266609093730dc781c1ddcdaff2403890c39b97f
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-08-24 21:59:41 -0300

    feat: add prisma beforeExit function

--- src/database/repositories/prisma/index.ts
+++ src/database/repositories/prisma/index.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { log } from "@utils/log";
 
 const prisma = new PrismaClient();
+
+prisma.$on("beforeExit", async () => {
+    log("Database connection closed")
+    await prisma.$disconnect();
+    return;
+})
 
 export { prisma }
